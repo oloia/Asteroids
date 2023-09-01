109731175dd313ea7d5dfb7c6271ba21cdcb6882
olga_lihaia <olga.l@usenergysolutions.us>
2023-09-01 12:49:37 +0300

    small fix

--- app/api.ts
+++
@@ -1,6 +0,0 @@
-export const API = {
-  topPage: {
-    find: process.env.NEXT_PUBLIC_DOMAIN + '/neo/rest/v1/feed?start_date=',
-    byAlias: process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/byAlias/'
-  },
-};
