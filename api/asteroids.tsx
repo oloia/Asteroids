20075a4eb8cfa75967668bd029e01e4e211581b7
olga_lihaia <olga.l@usenergysolutions.us>
2023-09-04 16:19:19 +0300

    small fix

--- api/asteroids.tsx
+++ api/asteroids.tsx
@@ -3,5 +3,8 @@ import { IRoot } from '@/interfaces/asteroid.interface';
 export async function getMenu(startDate: string): Promise<IRoot> {
   const res = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&api_key=${process.env.MY_KEY}`, {
   });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
   return res.json();
 }
--- api/detailPage.tsx
+++ api/detailPage.tsx
@@ -4,5 +4,8 @@ import { Root } from '@/interfaces/detailPage.interface';
 export async function getDetailPage(id: string): Promise<Root> {
   const res = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${process.env.MY_KEY}`, {
   });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
   return res.json();
 }
