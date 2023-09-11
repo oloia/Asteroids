b8bcf698167346e310b606cb2a75ba9e908236f0
olga_lihaia <olga.l@usenergysolutions.us>
2023-09-11 10:36:14 +0300

    Change Date

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { getMenu } from '@/api/asteroids';
 import { Card, Htag } from '@/components';
 import styles from '@/app/page.module.css';
-import { day, formattedDate } from '@/utils/FormatData';
 
 const Page = async () => {
-  const page = await getMenu(formattedDate(new Date()));
+  const page = await getMenu('2023-09-11');
   return (
     <div className={styles.title}>
       <Htag tag="h2">Ближайшие подлёты астероидов</Htag>
-      <Card hasBtn mainTitle={`${day} сент 2023`} description={page.near_earth_objects[formattedDate(new Date())]}/>
+      <Card hasBtn mainTitle={`11 сент 2023`} description={page.near_earth_objects['2023-09-11']}/>
     </div>
   );
 };
--- components/Order/OrderList.tsx
+++ components/Order/OrderList.tsx
@@ -3,14 +3,13 @@
 import { Card } from '@/components';
 import React from 'react';
 import { useCart } from '@/providers/CartProvider';
-import { day } from '@/utils/FormatData';
 
 const OrderList = async () => {
   const {state} = useCart();
 
   return (
     <Card
-      mainTitle={`${day} сент 2023`}
+      mainTitle={`11 сент 2023`}
       description={state}
     />
   );
